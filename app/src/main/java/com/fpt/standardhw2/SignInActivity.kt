95668938499a2a377d45fdf9d99e5e435187e68f
Sergei Putkin <s.putkin@iilab.ru>
2021-03-16 23:14:24 +0300

    Initial commit

---
+++ app/src/main/java/com/fpt/standardhw2/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.fpt.standardhw2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fpt.standardhw2.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signin_layout)
+    }
+}
